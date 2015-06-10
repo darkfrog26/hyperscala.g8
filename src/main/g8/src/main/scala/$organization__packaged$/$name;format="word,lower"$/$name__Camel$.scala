cc93140855b615f60a46bb7b1ceac49ef0d438b0
Matt Hicks <matt@outr.com>
2015-06-09 21:29:43 -0500

    Updated default version to be 1.0.0-SNAPSHOT and updated website to no longer use StaticWebsite.

--- src/main/g8/src/main/scala/$organization__packaged$/$name;format="word,lower"$/$name__Camel$.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/$name;format="word,lower"$/$name__Camel$.scala
@@ -3,6 +3,6 @@ package $organization$.$name;format="word,lower"$
 import org.hyperscala.web._
 import com.outr.net.http.jetty.JettyApplication
 
-object $name;format="Camel"$ extends BasicWebsite with StaticWebsite with JettyApplication {
-  def index = new HelloPage
+object $name;format="Camel"$ extends BasicWebsite with JettyApplication {
+  val hello = page(new HelloPage, Scope.Page, "/", "/index.html")
 }
